30d2838cd577eee53d53320c9c5efeb5629028e1
Hoon Lee <yl.hoony@gmail.com>
2017-11-01 19:28:04 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(args) # put argument(s) here
   # code here
+  i = 0
+
+  while i < args.length
+    yield(args[i])
+    i += 1
+  end
+  args
 end
+
+my_each(['arel', 'jon', 'logan', 'spencer']) { |arg| print "#{arg}" }
